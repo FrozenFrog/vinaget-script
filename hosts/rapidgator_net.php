afef4e51db758b103e96828568e60893e2b42e64
FrozenFrog <badauyeu4180@gmail.com>
2015-09-03 15:57:03 +0700

    Fix checkacc function rapidgator plugin Fix forget $this->lib

--- hosts/rapidgator_net.php
+++ hosts/rapidgator_net.php
@@ -3,31 +3,31 @@
 class dl_rapidgator_net extends Download {
 
 	public function CheckAcc($cookie){
-		$data = curl("http://rapidgator.net/profile/index", "lang=en;{$cookie}", "");
-		$account_type = cut_str(cut_str($data, "Account type</td>", '<tr>'), '<td>', '</td>');
+		$data = $this->lib->curl("http://rapidgator.net/profile/index", "lang=en;{$cookie}", "");
+		$account_type = $this->lib->cut_str(cut_str($data, "Account type</td>", '<tr>'), '<td>', '</td>');
 		if(stristr(trim($account_type), '<a class="orange" href="/article/premium">Upgrade to premium</a>')) return array(false, "accfree");
 		elseif(stristr(trim($account_type), "Premium")) {
-			$oob = curl("http://rapidgator.net/file/30703c88109d2a7b61a301eb6b324a95", "lang=en;{$cookie}", "");
-			if(stristr($oob, 'You have reached quota of downloaded information')) return array(true, "Until ".cut_str($data, 'Premium till','<span'). "<br> Account out of BW");
-			else return array(true, "Until ".cut_str($data, 'Premium till','<span')." <br/>Bandwith available:" .cut_str(cut_str($data, 'Bandwith available</td>','<div style='), '<td>','</br>'));
+			$oob = $this->lib->curl("http://rapidgator.net/file/30703c88109d2a7b61a301eb6b324a95", "lang=en;{$cookie}", "");
+			if(stristr($oob, 'You have reached quota of downloaded information')) return array(true, "Until ".$this->lib->cut_str($data, 'Premium till','<span'). "<br> Account out of BW");
+			else return array(true, "Until ".$this->lib->cut_str($data, 'Premium till','<span')." <br/>Bandwith available:" .$this->lib->cut_str(cut_str($data, 'Bandwith available</td>','<div style='), '<td>','</br>'));
 		}
 		else return array(false, "accinvalid");
 	}
-	
+
 	public function Login($user, $pass){
 		$data = $this->lib->curl("https://rapidgator.net/auth/login", "lang=en", "LoginForm[email]={$user}&LoginForm[password]={$pass}&LoginForm[rememberMe]=1");
 		$cookie = "lang=en;".$this->lib->GetCookies($data);
 		return $cookie;
 	}
-	
-    public function Leech($url) {
+
+	public function Leech($url) {
 		$data = $this->lib->curl($url,"lang=en;".$this->lib->cookie,"");
 		if(stristr($data, "You have reached quota of downloaded information") || stristr($data, "You have reached daily quota")) $this->error("LimitAcc");
 		elseif(stristr($data,'File not found</div>'))  $this->error("dead", true, false, 2);
 		elseif(preg_match('@https?:\/\/\w+\.rapidgator\.net\/\/\?r=download\/index[^"\'><\r\n\t]+@i', $data, $giay))
-		return trim($giay[0]);
+			return trim($giay[0]);
 		return false;
-    }
+	}
 
 }
 
